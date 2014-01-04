7dd720c087180c51ec207d689c23a5392df879e5
Jordan Santell <jsantell@gmail.com>
2014-01-03 22:26:07 -0800

    Use old JQ for < IE9

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,13 @@
 // Global deps
-if (typeof window === 'object')
-  global.$ = require('jquery')(window)
+
+// Include jQuery if we're in a browser environment
+// Use < 2.x.x if on IE8 or what
+if (typeof document === 'object') {
+  if (document.addEventListener)
+    global.$ = require('jquery')(window)
+  else
+    global.$ = require('./resources/jquery-1.10.2')(window);
+}
 global._ = require('underscore');
 global.Backbone = require('backbone');
 
