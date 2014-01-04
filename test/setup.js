b58ab1a5add23a24afd92b500df674e5be855e4d
Jordan Santell <jsantell@gmail.com>
2014-01-03 22:29:44 -0800

    Properly instantiate jQuery 1.10.2 via browserify for tests

--- test/setup.js
+++ test/setup.js
@@ -3,10 +3,10 @@
 // Include jQuery if we're in a browser environment
 // Use < 2.x.x if on IE8 or what
 if (typeof document === 'object') {
-  if (document.addEventListener)
-    global.$ = require('jquery')(window)
+    if (document.addEventListener)
+      global.$ = require('jquery')(window)
   else
-    global.$ = require('./resources/jquery-1.10.2')(window);
+    global.$ = require('./resources/jquery-1.10.2');
 }
 global._ = require('underscore');
 global.Backbone = require('backbone');
